a6940ffb41d9934acb92c916deb0a4d9bab8c6ef
Olha Pyshchyk <olhapyshchyk@gmail.com>
2023-05-20 21:14:21 +0200

    Projects update

--- pages/components/Projects/Projects.jsx
+++ pages/components/Projects/Projects.jsx
@@ -8,9 +8,15 @@ const Projects = () => {
       <h2 className={styles.projectsTitle}>Current projects</h2>
       <ul className={styles.projectsList}>
         <li>
+          <Image src="/frame.svg" width={14} height={14} alt="nature" style={{ marginRight: '4px' }} />
+          <span className={styles.description}>
+            Co-Built: <a className={styles.link} href="https://mockupgenerator.co" target="_blank" rel="noopener noreferrer" aria-label="mockupgenerator.co">MockupGenerator</a>, a simple tool to make mockups for art prints. (2023)
+          </span>
+        </li>
+        <li style={{ marginTop: '12px' }}>
           <Image src="/nature.svg" width={14} height={14} alt="nature" style={{ marginRight: '4px' }} />
           <span className={styles.description}>
-            Co-Building: <a className={styles.link} href="https://makeprintable.ai" target="_blank" rel="noopener noreferrer" aria-label="makeprintable.ai">Make Printable</a>, a web app powered by AI for creating high-resolution printable assets. (2023)
+            Co-Built: <a className={styles.link} href="https://makeprintable.ai" target="_blank" rel="noopener noreferrer" aria-label="makeprintable.ai">Make Printable</a>, a web app powered by AI for creating high-resolution printable assets. (2023)
           </span>
         </li>
         <li style={{ marginTop: '12px' }}>
